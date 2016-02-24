c4e3e00e7025373b8f7ca7944014d1fefb349d2f
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-24 16:51:42 -0500

    new account function working, still need to complete login

--- index.js
+++ index.js
@@ -13,7 +13,24 @@ var User = sequelize.define('User', {
 console.log('enter R for a new account');
 console.log('enter L to login');
 
-prompt.get(['input'], function(err, result))
+prompt.get(['input'], function(err, result) {
+    if(result.input === 'R'){
+        prompt.get(['username', 'password'], function(err, newUser){
+            bcrypt.genSalt(10, function(err, salt){
+                bcrypt.hash(newUser.Password, salt, function(err, hashedPassword){
+                    User.create({
+                        username: newUser.Username,
+                        password: hashedPassword
+                    }).then(function(){
+                        console.log('SAVED!');
+                    });
+                });
+            });
+        });
+    } else {
+        
+    }
+});
 
 
 
